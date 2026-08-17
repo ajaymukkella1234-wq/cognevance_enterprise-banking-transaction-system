3877b2453c83c2b1f40b0af0ff1b6679fb3506c5
ajay123 <ajaymukkella1234@gmail.com>
2026-08-17 22:27:07 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,17 @@
 
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        BankingSystem bank = new BankingSystem();
+        bank.login();
+
+        Account account = new Account();
+
+        account.deposit(5000);
+
+        Transaction transaction = new Transaction();
+        transaction.transfer(2000);
+
+        System.out.println("Current Balance: " + account.balance);
+    }
+}
