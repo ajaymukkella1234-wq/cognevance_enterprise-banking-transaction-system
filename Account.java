e7bd55c6d2c17ded5d6d8a51d4d3649b64afaef9
ajay123 <ajaymukkella1234@gmail.com>
2026-08-17 22:26:06 +0530

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,1 +1,13 @@
 
+// Account.java
+class Account {
+    double balance = 10000;
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+}
